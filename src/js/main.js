10cd02c86f66e7a8e1f47b463a48d5d74c410dac
Brian O'Toole <brian@sparxoo.com>
2018-03-19 18:27:09 -0400

    about + project

--- src/js/main.js
+++ src/js/main.js
@@ -29,17 +29,16 @@ $(function() {
 
 // when hover over work-item, take it's data-src attr and set it as imgSrc's img attr
 var workItem = $('.recent-work-list--is-home > li a');
+var breakpoint = 800;
 workItem.on({
 	mouseenter: function () {
 
-		var workItemSrc = $(this).data('src');
 		var imgContainer = $('.work-item-image img')
-		var imgSrc = imgContainer.attr('src');
 		var currentSrc = $(this).data('src');
-		var breakpoint = 800;
 		if ($(window).width() >= breakpoint) {
 			imgContainer.attr('src', currentSrc);
 			$(this).addClass('active');
+			imgContainer.addClass('active');
 		}
 	}
 });
