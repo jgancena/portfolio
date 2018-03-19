f68dd1f2bfaca943e5850ef8cb3adb5043b6ecee
Brian O'Toole <brian@sparxoo.com>
2018-03-19 19:35:40 -0400

    setup anims

--- src/js/tweens.js
+++ src/js/tweens.js
@@ -1,53 +1,45 @@
 //TimelineMax & ScrollMagic
-import ScrollMagic from 'scrollmagic/scrollmagic/uncompressed/ScrollMagic';   
-//import { TweenMax } from 'gsap';
+//import ScrollMagic from 'scrollmagic/scrollmagic/uncompressed/ScrollMagic';   
+import { TweenMax } from 'gsap';
 // TweenMax
 //tl.method(element, duration, vars), delay
-/*
+
 var tl = new TimelineMax({});
+var navItems = $('.nav-primary li');
+var workItems = $('.recent-work-list li');
+var sectionIntro = $('.section-intro')
 tl
-  // news page: timeline 
-	.staggerFrom('.post-item', 0.5, {y: 5,autoAlpha:0,ease: Power1.easeOut}, 0.15)
-	//.to('path.svg-web-gear', 5, {rotation:360,transformOrigin:'50% 50%', ease:Linear.easeNone, repeat:-1})
-*/
-
-// ScrollMagic
-// Init
-var controller = new ScrollMagic.Controller();
-
-//pin the intro
-var pinIntroScene = new ScrollMagic.Scene({
-	triggerElement: '.section-hero',
-	triggerHook: 0,
-	duration: '100%'
-})
-.setPin('.section-hero', {pushFollowers: false})
-.addTo(controller);
-
-//loop through each el and build scene
-$('.hero-content').each(function() {
-	//build a scene
-	var projectScene = new ScrollMagic.Scene({
-		//scene options
-		triggerElement: this.children[0],
-		triggerHook: 0.9,
-		reverse: false
+	// nav items
+	navItems.each(function() {
+    tl.add(
+      TweenMax.from($(this), 0.2, {
+        autoAlpha: 0,
+        x: 30,
+        ease: Linear.easeNone
+      })
+    );
 	})
-	.setClassToggle(this, 'fadeIn') //add class to el
-	//.addIndicators({name: 'fade scene',}) //indicators; uses plugin
-	.addTo(controller);
-});
-
-//loop through each el and build scene
-$('.svg').each(function() {
-	//build a scene
-	var projectScene = new ScrollMagic.Scene({
-		//scene options
-		triggerElement: this,
-		triggerHook: 0.9,
-		reverse: false
+	// home items
+	sectionIntro.each(function() {
+    tl.add(
+      TweenMax.staggerFrom($(this), 0.2, {
+        autoAlpha: 0,
+        y: 5,
+        ease: Power1.easeOut
+      })
+    );
 	})
-	.setClassToggle(this, 'active') //add class to el
-	//.addIndicators({name: 'svg scene',}) //indicators; uses plugin
-	.addTo(controller);
-});
+
+	// work items
+	workItems.each(function() {
+    tl.add(
+      TweenMax.staggerFrom($(this), 0.1, {
+        autoAlpha: 0,
+        y: 5,
+        ease: Power1.easeOut
+      })
+    );
+	})
+
+
+
